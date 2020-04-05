1eed87fd64b40669c8904456bed8cf47dafab3e6
tchen <tchen@tony.com>
2020-04-05 11:14:27 -0500

    Web App complete and ready for deployment

--- Portfolio Projects/2. Regression Analysis and Web App - Used Car Prices Prediction/Web App/app.py
+++ Portfolio Projects/2. Regression Analysis and Web App - Used Car Prices Prediction/Web App/app.py
@@ -16,14 +16,14 @@ choices_all = joblib.load(os.path.join(cur_dir, 'joblib_objects', 'choices.jobli
 
 #Build Form
 class EntryForm(Form):
-    year = fields.SelectField('Year', choices = choices_all['choices_years'], coerce = int, validators = [validators.InputRequired('Please select a value.')])
-    manufacturer = fields.SelectField('Manufacturer', choices = choices_all['choices_manufacturers'], validators = [validators.InputRequired('Please select a value.')])
-    model = fields.SelectField('Model', choices = choices_all['choices_models'], validators = [validators.InputRequired('Please select a value.')])
-    fuel = fields.SelectField('Fuel', choices = choices_all['choices_fuels'], validators = [validators.InputRequired('Please select a value.')])
-    odometer = fields.IntegerField('Odometer', validators = [validators.InputRequired('Please input a value.'), validators.NumberRange(min=10, max=250000, message='The value must be between 10 and 250000.')])
-    title_status = fields.SelectField('Title_status', choices = choices_all['choices_title_status'], validators = [validators.InputRequired('Please select a value.')])
-    transmission = fields.SelectField('Transmission', choices = choices_all['choices_transmissions'], validators = [validators.InputRequired('Please select a value.')])
-    state = fields.SelectField('State', choices = choices_all['choices_states'], validators = [validators.InputRequired('Please select a value.')])
+    year = fields.SelectField('Year', choices = choices_all['choices_years'], validators = [validators.InputRequired()])
+    manufacturer = fields.SelectField('Manufacturer', choices = choices_all['choices_manufacturers'], validators = [validators.InputRequired()])
+    model = fields.SelectField('Model', choices = choices_all['choices_models'], validators = [validators.InputRequired()])
+    fuel = fields.SelectField('Fuel', choices = choices_all['choices_fuels'], validators = [validators.InputRequired()])
+    odometer = fields.IntegerField('Odometer', validators = [validators.InputRequired(), validators.NumberRange(min=10, max=250000, message='The value must be between 10 and 250000.')])
+    title_status = fields.SelectField('Title Status', choices = choices_all['choices_title_status'], validators = [validators.InputRequired()])
+    transmission = fields.SelectField('Transmission', choices = choices_all['choices_transmissions'], validators = [validators.InputRequired()])
+    state = fields.SelectField('State', choices = choices_all['choices_states'], validators = [validators.InputRequired()])
 
 #index page
 @app.route('/')
@@ -34,18 +34,18 @@ def index():
 @app.route('/results', methods = ['POST'])
 def results():
     form = EntryForm(request.form)
-    if request.method = 'POST' and form.validate():
-        year = request.form['Year']
-        manufacturer = request.form['Manufacturer']
-        model = request.form['Model']
-        fuel = request.form['Fuel']
-        odometer = request.form['Odometer']
-        title_status = request.form['Title_status']
-        transmission = request.form['Transmission']
-        state = request.form['State']
+    if request.method == 'POST' and form.validate():
+        year = int(request.form['year'])
+        manufacturer = request.form['manufacturer']
+        model = request.form['model']
+        fuel = request.form['fuel']
+        odometer = int(request.form['odometer'])
+        title_status = request.form['title_status']
+        transmission = request.form['transmission']
+        state = request.form['state']
         X = pd.DataFrame({'year': year, 'manufacturer': manufacturer, 'model': model, 
                     'fuel': fuel, 'odometer': odometer, 'title_status': title_status,
-                    'transmission': transmission, 'state': state})
+                    'transmission': transmission, 'state': state}, index = [0])
         pred = round(estimator.predict(X)[0], 2)
         return render_template('results.html', content = request.form, price = pred)
     return render_template('index.html', form = form)
