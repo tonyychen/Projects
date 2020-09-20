f7211274b0a29c5c1ae7deaea48a0391661108bc
tchen <tchen@tony.com>
2020-09-20 08:57:09 -0500

    Modified the folder structure for TripAdvisor Project

--- TripAdvisor/Scraper/Scraper/spiders/tripadvisor.py
+++ TripAdvisor/Scraper/Scraper/spiders/tripadvisor.py
@@ -3,7 +3,7 @@ import scrapy
 
 class TripadvisorSpider(scrapy.Spider):
     name = 'tripadvisor'
-    allowed_domains = ['https://www.tripadvisor.com/']
+    allowed_domains = ['tripadvisor.com/']
     start_urls = ['http://https://www.tripadvisor.com//']
 
     def parse(self, response):
