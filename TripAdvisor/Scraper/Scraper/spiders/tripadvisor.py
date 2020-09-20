81c668ff48cce88c68ee8b9095c5addb4c569cf7
tchen <tchen@tony.com>
2020-09-20 10:18:08 -0500

    Started adding some lua script for scraping

--- TripAdvisor/Scraper/Scraper/spiders/tripadvisor.py
+++ TripAdvisor/Scraper/Scraper/spiders/tripadvisor.py
@@ -4,7 +4,7 @@ import scrapy
 class TripadvisorSpider(scrapy.Spider):
     name = 'tripadvisor'
     allowed_domains = ['tripadvisor.com/']
-    start_urls = ['http://https://www.tripadvisor.com//']
+    start_urls = ['https://www.tripadvisor.com/Attraction_Review-g60709-d12785836-Reviews-Acadia_National_Park-Bar_Harbor_Mount_Desert_Island_Maine.html']
 
     def parse(self, response):
         pass
