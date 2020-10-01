8d71b0c4031ff3df6c4e9336d900ce56193c69dc
tchen <tchen@tony.com>
2020-09-30 20:47:24 -0500

    start writing spider, the direction is on window.__WEB_CONTEXT__

--- TripAdvisor/Scraper/Scraper/spiders/tripadvisor.py
+++ TripAdvisor/Scraper/Scraper/spiders/tripadvisor.py
@@ -3,8 +3,11 @@ import scrapy
 
 class TripadvisorSpider(scrapy.Spider):
     name = 'tripadvisor'
-    allowed_domains = ['tripadvisor.com/']
+    allowed_domains = ['tripadvisor.com']
     start_urls = ['https://www.tripadvisor.com/Attraction_Review-g60709-d12785836-Reviews-Acadia_National_Park-Bar_Harbor_Mount_Desert_Island_Maine.html']
 
     def parse(self, response):
-        pass
+        for review in response.xpath('//q[@class="IRsGHoPm"]/span/text()'):
+            yield {
+                'text': review.get()
+            }
