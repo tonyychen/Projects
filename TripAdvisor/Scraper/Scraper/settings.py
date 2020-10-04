707222b88f174db7b3bc0c2408fdc5e8d909d4cf
tchen <tchen@tony.com>
2020-10-04 10:01:11 -0500

    Changed direction by using Regex to parse out JavaScript that contains review info and outputs JSON, next step to use JSON to parse out useful information on reviews and others

--- TripAdvisor/Scraper/Scraper/settings.py
+++ TripAdvisor/Scraper/Scraper/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = 'Scraper.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'Scraper (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -86,19 +86,3 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
-
-SPLASH_URL = 'http://localhost:8050'
-
-DOWNLOADER_MIDDLEWARES = {
-    'scrapy_splash.SplashCookiesMiddleware': 723,
-    'scrapy_splash.SplashMiddleware': 725,
-    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
-}
-
-SPIDER_MIDDLEWARES = {
-    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
-}
-
-DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
-
-HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
--- TripAdvisor/Scraper/Scraper/spiders/tripadvisor.py
+++ TripAdvisor/Scraper/Scraper/spiders/tripadvisor.py
@@ -1,29 +1,20 @@
 import scrapy
-from scrapy_splash import SplashRequest
+import re
 
 class TripadvisorSpider(scrapy.Spider):
     name = 'tripadvisor'
     allowed_domains = ['tripadvisor.com']
-    start_urls = ['https://www.tripadvisor.com/Attraction_Review-g60709-d12785836-Reviews-Acadia_National_Park-Bar_Harbor_Mount_Desert_Island_Maine.html']
-
-    script = '''
-    function main(splash, args)
-        splash.private_mode_enabled = false
-
-        url = args.url
-        assert(splash:go(url))
-        assert(splash:wait(1))
-        
-        splash:set_viewport_full()
-        return splash.html()
-    end
-    '''
-
-    def start_requests(self):
-        yield SplashRequest(url = self.start_urls[0], callback=self.parse, endpoint='execute', args={
-            'lua_source': self.script
-        })
+    start_urls = ['https://www.tripadvisor.com/Attraction_Review-g60709-d12785836-Reviews-Acadia_National_Park-Bar_Harbor_Mount_Desert_Island_Maine.html#REVIEWS']
 
     def parse(self, response):
-        with open('testfile.txt', 'wb') as f:
-            f.write(response.body)
+        js = response.xpath('//script[starts-with(text(), "window.__WEB_CONTEXT__")]/text()').get()
+        #remove start
+        js = re.sub(r'^window\.__WEB_CONTEXT__\s*=\s*{', r'{', js)
+        #remove end
+        js = re.sub(r'};\(this\.\$WP.+$', r'}', js)
+        #double-quote "pageManifest"
+        js = js.replace('pageManifest', '"pageManifest"')
+
+        #output json
+        with open('testfile.json', 'w') as f:
+            f.write(js)
