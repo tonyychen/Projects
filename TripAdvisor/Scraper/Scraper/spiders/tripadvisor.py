f15a3af0945a3d14dc2f8ab64e87c3e351dd577c
tchen <tchen@tony.com>
2020-10-04 19:58:31 -0500

    additional logic to find specific review sections in JSON

--- TripAdvisor/Scraper/Scraper/spiders/tripadvisor.py
+++ TripAdvisor/Scraper/Scraper/spiders/tripadvisor.py
@@ -1,5 +1,7 @@
 import scrapy
 import re
+import json
+
 
 class TripadvisorSpider(scrapy.Spider):
     name = 'tripadvisor'
@@ -7,14 +9,41 @@ class TripadvisorSpider(scrapy.Spider):
     start_urls = ['https://www.tripadvisor.com/Attraction_Review-g60709-d12785836-Reviews-Acadia_National_Park-Bar_Harbor_Mount_Desert_Island_Maine.html#REVIEWS']
 
     def parse(self, response):
-        js = response.xpath('//script[starts-with(text(), "window.__WEB_CONTEXT__")]/text()').get()
-        #remove start
+        js = response.xpath(
+            '//script[starts-with(text(), "window.__WEB_CONTEXT__")]/text()').get()
+        # clean up start
         js = re.sub(r'^window\.__WEB_CONTEXT__\s*=\s*{', r'{', js)
-        #remove end
+        # clean up end
         js = re.sub(r'};\(this\.\$WP.+$', r'}', js)
-        #double-quote "pageManifest"
+        # add double-quote to "pageManifest" to make it a key
         js = js.replace('pageManifest', '"pageManifest"')
 
-        #output json
+        # only get the part where key = "reviewListPage", which contains review info
+        js = json.loads(js)
+        js = self.json_extract(js, 'reviewListPage')[0] #only 1 "reviewListPage" should exist
+
+        # output json
         with open('testfile.json', 'w') as f:
-            f.write(js)
+           json.dump(js, f)
+
+    def json_extract(self, obj, key):
+        """Extract nested values from a JSON tree."""
+        """Recursively fetch values from nested JSON."""
+        arr = []
+
+        def extract(obj, arr, key):
+            """Recursively search for values of key in JSON tree."""
+            if isinstance(obj, dict):
+                for k, v in obj.items():
+                    if k == key:
+                        arr.append(v)
+                        
+                    if isinstance(v, (dict, list)):
+                        extract(v, arr, key)
+            elif isinstance(obj, list):
+                for item in obj:
+                    extract(item, arr, key)
+            return arr
+
+        values = extract(obj, arr, key)
+        return values
