4b513310ea5d92f473726f8b0ce3a04c52acea3f
tchen <chent99@outlook.com>
2020-11-08 10:32:35 -0600

    Scrapy pipelines etc.
    1. Moved spider transformation logic to Scrapy pipeline
    2. Extracted the latest data using the pipeline then rerun the notebooks

--- Portfolio Projects/TripAdvisor/Scraper/Scraper/pipelines.py
+++ Portfolio Projects/TripAdvisor/Scraper/Scraper/pipelines.py
@@ -6,8 +6,44 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import re
+import json
 
 class ScraperPipeline:
+    def json_extract(self, obj, key):
+        """Extract nested values from a JSON tree."""
+        """Recursively fetch values from nested JSON."""
+        arr = []
+
+        def extract(obj, arr, key):
+            """Recursively search for values of key in JSON tree."""
+            if isinstance(obj, dict):
+                for k, v in obj.items():
+                    if k == key:
+                        arr.append(v)
+                        
+                    if isinstance(v, (dict, list)):
+                        extract(v, arr, key)
+            elif isinstance(obj, list):
+                for item in obj:
+                    extract(item, arr, key)
+            return arr
+
+        values = extract(obj, arr, key)
+        return values
+
     def process_item(self, item, spider):
-        return item
+        js = item['JS']
+        # clean up start
+        js = re.sub(r'^window\.__WEB_CONTEXT__\s*=\s*{', r'{', js)
+        # clean up end
+        js = re.sub(r'};\(this\.\$WP.+$', r'}', js)
+        # add double-quote to "pageManifest" to make it a key
+        js = js.replace('pageManifest', '"pageManifest"')
+
+        # only get the part where key = "reviewListPage", which contains review info
+        js = json.loads(js)
+        js = self.json_extract(js, 'reviewListPage')[0] #only 1 "reviewListPage" should exist
+        js = json.dumps(js)
+
+        return {'Review_JSON': js}
--- Portfolio Projects/TripAdvisor/Scraper/Scraper/spiders/tripadvisor.py
+++ Portfolio Projects/TripAdvisor/Scraper/Scraper/spiders/tripadvisor.py
@@ -1,6 +1,4 @@
 import scrapy
-import re
-import json
 
 
 class TripadvisorSpider(scrapy.Spider):
@@ -11,21 +9,10 @@ class TripadvisorSpider(scrapy.Spider):
     def parse(self, response):
         js = response.xpath(
             '//script[starts-with(text(), "window.__WEB_CONTEXT__")]/text()').get()
-        # clean up start
-        js = re.sub(r'^window\.__WEB_CONTEXT__\s*=\s*{', r'{', js)
-        # clean up end
-        js = re.sub(r'};\(this\.\$WP.+$', r'}', js)
-        # add double-quote to "pageManifest" to make it a key
-        js = js.replace('pageManifest', '"pageManifest"')
-
-        # only get the part where key = "reviewListPage", which contains review info
-        js = json.loads(js)
-        js = self.json_extract(js, 'reviewListPage')[0] #only 1 "reviewListPage" should exist
-        js = json.dumps(js)
 
         # output json
         yield {
-            'Review_JSON': js
+            'JS': js
         }
 
         #Pagination (next page)
@@ -33,24 +20,4 @@ class TripadvisorSpider(scrapy.Spider):
         if next_page:
             yield response.follow(next_page, callback=self.parse)
 
-    def json_extract(self, obj, key):
-        """Extract nested values from a JSON tree."""
-        """Recursively fetch values from nested JSON."""
-        arr = []
 
-        def extract(obj, arr, key):
-            """Recursively search for values of key in JSON tree."""
-            if isinstance(obj, dict):
-                for k, v in obj.items():
-                    if k == key:
-                        arr.append(v)
-                        
-                    if isinstance(v, (dict, list)):
-                        extract(v, arr, key)
-            elif isinstance(obj, list):
-                for item in obj:
-                    extract(item, arr, key)
-            return arr
-
-        values = extract(obj, arr, key)
-        return values
