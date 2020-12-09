d1e8224b6ad3a4af4195932bad8a829a00942a64
tchen <chent99@outlook.com>
2020-12-09 08:56:30 -0600

    TripAdivsor Project
    Prepare to crawl all national parks available

---
+++ Portfolio Projects/TripAdvisor/Scraper/Scraper/spiders/nationalpark_urls.py
@@ -0,0 +1,18 @@
+park_urls = [
+    #Acadia
+    'https://www.tripadvisor.com/Attraction_Review-g60709-d12785836-Reviews-Acadia_National_Park-Bar_Harbor_Mount_Desert_Island_Maine.html',
+    #American Samoa 
+    'https://www.tripadvisor.com/Attraction_Review-g60672-d6776101-Reviews-National_Park_of_American_Samoa-Pago_Pago_Tutuila.html',
+    #Arches
+    'https://www.tripadvisor.com/Attraction_Review-g60724-d8535374-Reviews-Arches_National_Park-Moab_Utah.html',
+    #Badlands
+    'https://www.tripadvisor.com/Attraction_Review-g60729-d10212205-Reviews-Badlands_National_Park-Interior_South_Dakota.html',
+    #Big Bend: N/A
+    #Biscayne: N/A
+    #Black Canyon of the Gunnison
+    'https://www.tripadvisor.com/Attraction_Review-g143014-d145009-Reviews-Black_Canyon_Of_The_Gunnison_National_Park-Black_Canyon_Of_The_Gunnison_National_P.html',
+    #Bryance Canyon
+    'https://www.tripadvisor.com/Attraction_Review-g143015-d1925256-Reviews-Bryce_Canyon_National_Park-Bryce_Canyon_National_Park_Utah.html',
+    #Canyonlands
+    'https://www.tripadvisor.com/Attraction_Review-g60724-d8837806-Reviews-Canyonlands_National_Park-Moab_Utah.html'
+]
--- Portfolio Projects/TripAdvisor/Scraper/Scraper/spiders/tripadvisor.py
+++ Portfolio Projects/TripAdvisor/Scraper/Scraper/spiders/tripadvisor.py
@@ -1,10 +1,11 @@
 import scrapy
-
+from . import nationalpark_urls
 
 class TripadvisorSpider(scrapy.Spider):
     name = 'tripadvisor'
     allowed_domains = ['tripadvisor.com']
-    start_urls = ['https://www.tripadvisor.com/Attraction_Review-g60709-d12785836-Reviews-Acadia_National_Park-Bar_Harbor_Mount_Desert_Island_Maine.html#REVIEWS']
+    start_urls = nationalpark_urls.park_urls
+
 
     def parse(self, response):
         js = response.xpath(
